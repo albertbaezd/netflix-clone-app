3376666feb298a1b8f04d68180dd6008dbeccd8d
AlbertD <albertariel2013@hotmail.com>
2021-06-24 18:35:53 -0400

    Setting up initial files for the tutorial

---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container, Inner } from './styles/jumbotron';
+
+export default function Jumbotron({children, direction = 'row', ...restProps}) {
+    return (
+    <Inner direction = {direction}>
+        <p>Hello</p>
+    </Inner>)
+}
+
+Jumbotron.Container = function JumbotronContainer({children, ...restProps}) {
+    return <Container {...restProps}>{children}</Container>
+}
