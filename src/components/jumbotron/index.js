9f828df1098f8697ed283f8c1437d5b345c5b464
AlbertD <albertariel2013@hotmail.com>
2021-06-26 20:31:08 -0400

    Loaded images, videos + styles on main page

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
-import { Container, Inner } from './styles/jumbotron';
+import { Container, Inner, Pane, Title, SubTitle, Image } from './styles/jumbotron';
 
 export default function Jumbotron({children, direction = 'row', ...restProps}) {
     return (
     <Inner direction = {direction}>
-        <p>Hello</p>
+        {children}
     </Inner>)
 }
 
 Jumbotron.Container = function JumbotronContainer({children, ...restProps}) {
     return <Container {...restProps}>{children}</Container>
+}
+
+Jumbotron.Pane = function JumbotronPane({children, ...restProps}) {
+    return <Pane {...restProps}>{children}</Pane>
+}
+
+Jumbotron.Title = function JumbotronTitle({children, ...restProps}) {
+    return <Title {...restProps}>{children}</Title>
+}
+
+Jumbotron.SubTitle = function JumbotronSubTitle({children, ...restProps}) {
+    return <SubTitle {...restProps}>{children}</SubTitle>
+}
+
+Jumbotron.Image = function JumbotronImage({children, ...restProps}) {
+    return <Image {...restProps}/>
 }
