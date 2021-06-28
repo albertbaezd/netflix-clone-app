5aed520b844d8b3e6f1f75827667b2939c6400fc
AlbertD <albertariel2013@hotmail.com>
2021-06-27 20:47:36 -0400

    Initial changes for accordion component

---
+++ src/components/accordion/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Accordion ({children, ...restProps}) {
+    return (
+        <Container {...restProps}>
+            <Inner>{children}</Inner>
+        </Container>
+    )
+}
