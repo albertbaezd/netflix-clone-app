bf7a8f4be2742608248844f9f5829b87bf06f240
albertbaezd <albertariel2013@hotmail.com>
2021-07-16 17:09:53 -0400

    Add firebase logic to user registration with email

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -21,6 +21,26 @@ export default function Signup() {
   const handleSignUp = (event) => {
     event.preventDefault();
 
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(emailAddress, password)
+      .then((result) =>
+        result.user
+          .updateProfile({
+            displayName: firstName,
+            photoUrl: Math.floor(Math.random() * 5) + 1,
+          })
+          .then(() => {
+            history.push(ROUTES.BROWSE);
+          })
+      )
+      .catch((error) => {
+        setFirstName("");
+        setPassword("");
+        setEmailAddress("");
+        setError(error.message);
+      });
+
     //do firebase stuff
   };
   return (
