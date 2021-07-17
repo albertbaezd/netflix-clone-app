b02d1e5e6a422692ec8ed51a6826ce0676e85fe9
albertbaezd <albertariel2013@hotmail.com>
2021-07-17 00:50:44 -0400

    Setup helpers for signup and signin

--- src/App.js
+++ src/App.js
@@ -2,21 +2,36 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Home, Browse, Signin, Signup } from "./pages";
 import * as ROUTES from "./constants/routes";
+import { IsUserRedirect } from "./helpers/routes";
 //2:23:27
 function App() {
+  const user = {};
   return (
     <Router>
-      <Route exact path={ROUTES.HOME}>
-        <Home />
-      </Route>
+      <IsUserRedirect
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        path={ROUTES.SIGN_IN}
+        exact
+      >
+        <Signin />
+      </IsUserRedirect>
+
+      <IsUserRedirect
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        path={ROUTES.SIGN_UP}
+        exact
+      >
+        <Signup />
+      </IsUserRedirect>
+
       <Route exact path="/browse">
         <Browse />
       </Route>
-      <Route exact path="/signin">
-        <Signin />
-      </Route>
-      <Route exact path="/signup">
-        <Signup />
+
+      <Route exact path={ROUTES.HOME}>
+        <Home />
       </Route>
     </Router>
   );
---
+++ src/helpers/routes.js
@@ -0,0 +1,22 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+
+export function IsUserRedirect({ user, loggedInPath, children, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={() => {
+        if (!user) {
+          return children;
+        }
+
+        if (user) {
+          <Redirect to={{ pathname: loggedInPath }} />;
+        }
+
+        return null;
+      }}
+    ></Route>
+  );
+}
