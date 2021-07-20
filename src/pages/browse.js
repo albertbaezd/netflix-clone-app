f1a9477cd43a8bb1892d2f77dbab0f135efc6a42
albertbaezd <albertariel2013@hotmail.com>
2021-07-20 18:04:04 -0400

    Setting and testing use-content hook

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,5 +1,8 @@
 import React from "react";
+import { useContent } from "../hooks";
 
 export default function Browse() {
+  //we need series and films
+  const { series } = useContent("series");
   return <p>Hello from the Browse</p>;
 }
