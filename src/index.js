62a3c4026f977130ede5dff12f43d0bb1b44992e
albertbaezd <albertariel2013@hotmail.com>
2021-08-15 17:20:32 -0400

    Fix filename on index js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "normalize.css";
-import App from "./app";
+import App from "./app.js";
 import { GlobalStyles } from "./global-styles";
 import { firebase } from "./lib/firebase.prod";
 import { FirebaseContext } from "./context/firebase";
